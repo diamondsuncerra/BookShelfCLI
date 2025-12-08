7c2ea2b9e8801a018430608cc82a3cc6284f5e9b
Cerasela-Nicola Soare <ceraselanicola@yahoo.com>
2025-12-08 14:00:45 +0200

    Added Book.

---
+++ BookShelf.Domain/Books/Book.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.Domain.Books
+{
+    public abstract class Book
+    {
+        public Guid Id { get; protected set; }
+        public string Title { get; protected set; }
+        public string Author { get; protected set; }
+        public int Year { get; protected set; }
+        public DateTimeOffset AddedAt { get; protected set; }
+
+        public Book(string title, string author, int year)
+        {
+            Title = ValidateText(title);
+            Author = ValidateText(author);
+            Year = ValidateYear(year);
+        }
+
+        private int ValidateYear(int year)
+        {
+            int currentLimit = DateTimeOffset.UtcNow.Year + 1;
+            if (year < 1450 || year > currentLimit)
+                throw new ArgumentException($"Year must be in range 1450..{currentLimit}.");
+            return year;
+        }
+
+        private static string ValidateText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new Exception("Field cannot be empty.");
+            }
+            else return text.Trim();
+        }
+
+
+    }
+}
